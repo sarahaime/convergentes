e112c512855d47105ec2bd98ae18f8309dfac836
Sarahaime Rodríguez <saraahr_@hotmail.com>
2018-08-01 12:51:27 -0400

    practica testeada

--- routes/index.js
+++ routes/index.js
@@ -1,39 +1,38 @@
-var express = require('express');
+var express= require('express');
 var router = express.Router();
+var mysql  = require('mysql');
 
-var mysql      = require('mysql');
 var connection = mysql.createConnection({
     host     : '127.0.0.1',
     user     : 'root',
-    password : 'sarahaime123',
+    password : 'guillermo1',
     database : 'numeros'
 });
 
 connection.connect();
-
-connection.query('SELECT cnat from pizzahut', function(err, rows, fields) {
-    if (!err)
-        console.log('The solution is pizzahut: ', rows);
-    else
-        console.log('Error while performing Query.');
+connection.on('connect', function() {
+    console.log("Conectado con la DB");
 });
-connection.end();
-
-connection.connect();
-
-connection.query('SELECT cnat from dominos', function(err, rows, fields) {
-    if (!err)
-        console.log('The solution is dominos: ', rows);
-    else
-        console.log('Error while performing Query.');
-});
-connection.end();
 
 
-
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.redirect("/resultados.html");
+});
+
+router.get('/encuesta', function(req, res, next) {
+    let ans = [];
+    /***Callback de la muerte**********/
+    connection.query('SELECT max(cnat) as cantidad from dominos', function (err, rows, fields) {
+        ans.push(  { label: "Dominos",  y: parseInt(rows[0].cantidad)  ,  indexLabel: rows[0].cantidad.toString() } );
+        connection.query('SELECT max(cnat) as cantidad from pizzahut', function (err, rows, fields) {
+            ans.push(  { label: "Pizza Hut",  y: parseInt(rows[0].cantidad)  ,  indexLabel: rows[0].cantidad.toString() } );
+            connection.query('SELECT max(cnat) as cantidad from papajohn', function (err, rows, fields) {
+                ans.push(  { label: "Papa Johns",  y: parseInt(rows[0].cantidad)  ,  indexLabel: rows[0].cantidad.toString()} );
+                res.json(ans);
+            });
+        });
+    });
+
 });
 
 
